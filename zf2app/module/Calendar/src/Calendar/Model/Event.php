1a2213358cf274d5230f90e2317a895ff0e52ac7
Shahar Evron <shahar.evron@gmail.com>
2012-07-01 18:38:49 +0300

    improving look of event list

--- zf2app/module/Calendar/src/Calendar/Model/Event.php
+++ zf2app/module/Calendar/src/Calendar/Model/Event.php
@@ -6,5 +6,19 @@ use Zend\Db\ResultSet\Row;
 
 class Event extends Row
 {
+    protected $startDateTime = null;
 
+    /**
+     * Get the start_at value as a DateTime object
+     *
+     * @return DateTime
+     */
+    public function getStartDateTime()
+    {
+        if ($this->start_at && $this->startDateTime === null) {
+            $this->startDateTime = \DateTime::createFromFormat("Y-m-d H:i:s", $this->start_at);
+        }
+
+        return $this->startDateTime;
+    }
 }
